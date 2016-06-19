890400e83a69df1dfe4af1d4b100ad76baed3f4c
VinsunB <vinsuncoding@gmail.com>
2016-06-19 07:19:28 -0400

    basic blog

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,48 @@
+class ArticlesController < ApplicationController
+  
+  before_action :find_article, only: [:show, :edit, :update]
+
+  def new
+  @article = Article.new
+  end
+
+def edit
+end
+
+  def show
+  end
+
+  def index
+  @articles = Article.all
+  end
+
+def create 
+@article = Article.create(article_params)
+
+if @article.save
+redirect_to @article
+else
+	redirect_to articles_path
+end
+ end
+
+def update
+@article.update(article_params)
+if @article.save
+redirect_to @article
+ else
+  redirect_to articles_path
+ end
+end
+
+private
+
+def find_article
+@article = Article.find(params[:id])
+end 
+
+def article_params
+params.require(:article).permit(:title, :content)
+end
+
+end
