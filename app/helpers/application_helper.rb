f63b62c66d7ec6fb872e7fa44c2fbfe4a48c3b93
VinsunB <vinsuncoding@gmail.com>
2016-06-25 05:37:11 -0400

    friendly_id gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
 	def sanitizer(var)
-	sanitize(var, tags: ['strong', 'em', 'a', 'div','br', 'p', 'h1','h2','h3','h4','h5','h6','ol','ul','li'], attributes: ['href', 'class'])
+	tags = ['strong', 'em', 'a', 'div','br', 'p', 'h1','h2','h3','h4','h5','h6','ol','ul','li']
+	sanitize(var, tags: tags, attributes: ['href', 'class'])
 	end
 end
